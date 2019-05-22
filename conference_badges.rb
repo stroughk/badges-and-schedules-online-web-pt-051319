7240548380973c4e4f5d735823773d8b4c90b3e7
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-22 23:16:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index do |name, index|
+  attendees.each_with_index.map do |name, index|
     return "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
 end
